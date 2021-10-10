0863f507a68f278f21a8fa2b92205c91105658e3
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-10-10 23:32:23 +0200

    Convert patcher to new Experience version with ini file structure

--- ExperienceMutagenPatcher/Program.cs
+++ ExperienceMutagenPatcher/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using Mutagen.Bethesda.Plugins;
 
 namespace ExperienceMutagenPatcher
 {
@@ -26,17 +27,31 @@ namespace ExperienceMutagenPatcher
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var csv = new StringBuilder();
-            foreach (var race in state.LoadOrder.PriorityOrder.Race().WinningOverrides())
+            Dictionary<ModKey, List<string>> generatedData = new Dictionary<ModKey, List<string>>();
+            var ini = new StringBuilder();
+            foreach (var raceGetter in state.LoadOrder.PriorityOrder.Race().WinningContextOverrides())
             {
+                var race = raceGetter.Record;
                 float startingHealth = race.Starting.Values.ElementAt(0);
+                if (!generatedData.ContainsKey(raceGetter.ModKey))
+                    generatedData.Add(raceGetter.ModKey, new List<string>());
+
                 // Not using Math.Round since the result will be different from JS Math.round
                 // Using this trick from https://stackoverflow.com/questions/1862992/how-close-is-the-javascript-math-round-to-the-c-sharp-math-round (second answer
-                csv.Append(race.EditorID + "," + Math.Floor((startingHealth / 10) + 0.5) + '\n');
+                generatedData[raceGetter.ModKey].Add($"{race.EditorID}={Math.Floor((startingHealth / 10) + 0.5)}");
             }
+
+            generatedData = generatedData.OrderBy(kv => kv.Key.Name).ToDictionary();
+            foreach (var (modKey, linesToOutput) in generatedData)
+            {
+                ini.AppendLine($"[{modKey.FileName}]");
+                linesToOutput.ForEach(line => ini.AppendLine(line));
+                ini.AppendLine();
+            }
+
             loadPath = state.DataFolderPath;
             Directory.CreateDirectory(state.DataFolderPath + @"\SKSE\Plugins\Experience\Races\");
-            File.WriteAllText(state.DataFolderPath + @"\SKSE\Plugins\Experience\Races\experiencePatch.csv", csv.ToString());
+            File.WriteAllText(state.DataFolderPath + @"\SKSE\Plugins\Experience\Races\experiencePatch.ini", ini.ToString());
         }
     }
 }
